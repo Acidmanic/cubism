b1761bab196e55da29246a11724d708cc4a80f4e
Mani Moayedi <acidmanic.moayedi@gmail.com>
2026-06-16 11:31:46 +0330

    add extension methods for 2d rect

---
+++ Cubism/Extensions/RenderContextExtensions.cs
@@ -0,0 +1,27 @@
+using Cubism.Abstractions;
+using Cubism.Models;
+
+namespace Cubism.Extensions;
+
+public static class RenderContextExtensions
+{
+    public static void DrawRectangle(this IRenderContext ctx, System.Drawing.Color color, Point2 topLeft, double width, double height)
+    {
+        ctx.DrawPolygon(color, [
+            topLeft,
+            new Point2(topLeft.X + width, topLeft.Y),
+            new Point2(topLeft.X + width, topLeft.Y + height),
+            new Point2(topLeft.X, topLeft.Y + height),
+        ]);
+    }
+
+    public static void FillRectangle(this IRenderContext ctx, System.Drawing.Color color, Point2 topLeft, double width, double height)
+    {
+        ctx.FillPolygon(color, [
+            topLeft,
+            new Point2(topLeft.X + width, topLeft.Y),
+            new Point2(topLeft.X + width, topLeft.Y + height),
+            new Point2(topLeft.X, topLeft.Y + height),
+        ]);
+    }
+}
